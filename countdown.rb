ab914c134e79ff56d3646a0ab584609d2026587b
MUHANNAD JABER <abujaber44@gmail.com>
2020-06-21 22:23:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  
+  x = 10 
+while x > 0 
+  puts "#{x} SECOND(S)!"
+  x -= 1
+end
+return "HAPPY NEW YEAR!"
+
+end 
+
+loop do
+  t = Time.now
+  countdown(5)
+  sleep(t + 1 - Time.now)
 end
